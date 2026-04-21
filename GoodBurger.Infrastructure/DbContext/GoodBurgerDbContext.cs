ac42c77179caaace35c6865245455e13a5b3f7ec
chmov <chmov@PC-CARLOSHMV>
2026-04-21 16:15:29 -0300

    feat: create DbContext

---
+++ GoodBurger.Infrastructure/DbContext/GoodBurgerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GoodBurger.Domain.Entities;
+
+namespace GoodBurger.Infrastructure;
+
+public class GoodBurgerDbContext : DbContext
+{
+    public GoodBurgerDbContext(DbContextOptions<GoodBurgerDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Order> Orders { get; set; }
+}
