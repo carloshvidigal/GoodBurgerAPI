452d109e7bf614746583596c94f70e261370fba7
chmov <chmov@PC-CARLOSHMV>
2026-04-21 16:09:14 -0300

    feat: register OrderService and configure SQLite DbContext

--- GoodBurger.API/Program.cs
+++ GoodBurger.API/Program.cs
@@ -1,3 +1,7 @@
+using GoodBurger.Application.Services;
+using GoodBurger.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<OrderService>();
+
+builder.Services.AddDbContext<GoodBurgerDbContext>(options => options.UseSqlite("Data Source=goodburger.db"));
 
 var app = builder.Build();
 
