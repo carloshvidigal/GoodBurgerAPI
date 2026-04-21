8c5c831e99ae33e99fec02f54d23a077f9a75e2e
chmov <chmov@PC-CARLOSHMV>
2026-04-21 14:28:34 -0300

    feat: create Service

---
+++ GoodBurger.Application/Services/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using GoodBurger.Application.DTOs;
+using GoodBurger.Domain.Entities;
+using GoodBurger.Domain.Enums;
+
+namespace GoodBurger.Application.Services;
+
+public class OrderService
+{
+    public OrderResponse CreateOrder(CreateOrderRequest request)
+    {
+        var order = new Order
+        {
+            Sandwich = (SandwichType)request.Sandwich,
+            HasFries = request.HasFries,
+            HasDrink = request.HasDrink
+        };
+
+        order.Calculate();
+
+        return new OrderResponse
+        {
+            Id = order.Id,
+            Subtotal = order.Subtotal,
+            Discount = order.Discount,
+            Total = order.Total
+        };
+    }
+}
