5a01a92d86e383e771aa79a8c8005d7ff7bd7696
chmov <chmov@PC-CARLOSHMV>
2026-04-21 20:25:32 -0300

    feat(api): implement GetAll, GetById, Update and Delete endpoints

--- GoodBurger.API/Controllers/OrdersController.cs
+++ GoodBurger.API/Controllers/OrdersController.cs
@@ -21,4 +21,32 @@ public class OrdersController : ControllerBase
         var result = _orderService.CreateOrder(request);
         return Ok(result);
     }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var result = _orderService.GetAll();
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(Guid id)
+    {
+        var result = _orderService.GetById(id);
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(Guid id, CreateOrderRequest request)
+    {
+        var result = _orderService.Update(id, request);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        _orderService.Delete(id);
+        return NoContent();
+    }
 }
--- GoodBurger.Application/Services/OrderService.cs
+++ GoodBurger.Application/Services/OrderService.cs
@@ -36,4 +36,67 @@ public class OrderService
             Total = order.Total
         };
     }
+
+    public List<OrderResponse> GetAll()
+    {
+        return _context.Orders.Select(order => new OrderResponse
+            {
+                Id = order.Id,
+                Subtotal = order.Subtotal,
+                Discount = order.Discount,
+                Total = order.Total
+            })
+            .ToList();
+    }
+
+    public OrderResponse GetById(Guid id)
+    {
+        var order = _context.Orders.FirstOrDefault(x => x.Id == id);
+
+        if (order == null)
+            throw new Exception("Order not found");
+
+        return new OrderResponse
+        {
+            Id = order.Id,
+            Subtotal = order.Subtotal,
+            Discount = order.Discount,
+            Total = order.Total
+        };
+    }
+
+    public OrderResponse Update(Guid id, CreateOrderRequest request)
+    {
+        var order = _context.Orders.FirstOrDefault(x => x.Id == id);
+
+        if (order == null)
+            throw new Exception("Order not found");
+
+        order.Sandwich = (SandwichType)request.Sandwich;
+        order.HasFries = request.HasFries;
+        order.HasDrink = request.HasDrink;
+
+        order.Calculate();
+
+        _context.SaveChanges();
+
+        return new OrderResponse
+        {
+            Id = order.Id,
+            Subtotal = order.Subtotal,
+            Discount = order.Discount,
+            Total = order.Total
+        };
+    }
+
+    public void Delete(Guid id)
+    {
+        var order = _context.Orders.FirstOrDefault(x => x.Id == id);
+
+        if (order == null)
+            throw new Exception("Order not found");
+
+        _context.Orders.Remove(order);
+        _context.SaveChanges();
+    }
 }
