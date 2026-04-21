41dc54c6632e6698a10d0559c7b580ab32c9dac0
chmov <chmov@PC-CARLOSHMV>
2026-04-21 14:28:15 -0300

    feat: create DTO

---
+++ GoodBurger.Application/DTOs/CreateOrderRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace GoodBurger.Application.DTOs;
+
+public class CreateOrderRequest
+{
+    public int Sandwich { get; set; }
+    public bool HasFries { get; set; }
+    public bool HasDrink { get; set; }
+}
