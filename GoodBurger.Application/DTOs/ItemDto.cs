160f72195dceb76d2ebe76d42a055da0795027db
chmov <chmov@PC-CARLOSHMV>
2026-04-22 14:40:24 -0300

    feat: implement itemDto and MenuResponse

---
+++ GoodBurger.Application/DTOs/ItemDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodBurger.Application.DTOs
+{
+    public class ItemDto
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ GoodBurger.Application/DTOs/MenuResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodBurger.Application.DTOs
+{
+    public class MenuResponse
+    {
+        public List<ItemDto> Sandwiches { get; set; }
+        public List<ItemDto> Extras { get; set; }
+    }
+}
